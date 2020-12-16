016e61d5c2518ca54bff294b772bada9b288c0d1
martinagabrielli <martina.gabrielli@yahoo.it>
2020-12-16 12:21:34 +0000

    added react import and useState

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import React, { useState } from 'react'
 import './App.css'
 const url = 'https://course-api.netlify.app/api/react-tours-project'
 
 function App() {
+  const [loading, setLoading] = useState(false)
   return (
     <div className="App">
       <h1>Our Tours</h1>
