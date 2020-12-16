a5cd7326f533608c77ab278f0fc39313d3a1d310
martinagabrielli <martina.gabrielli@yahoo.it>
2020-12-16 13:44:29 +0000

    added/removed components

---
+++ src/components/Tour.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react'
+
+const Tour = ({id, image, info, price, name}) => {
+    return (
+        <div>
+            Tour
+        </div>
+    )
+}
+
+export default Tour
